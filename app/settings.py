751dcefab753bc85dc6f4152212e9ba03a8a20be
Bastian Katzmann <bastian.katzmann@web.de>
2024-06-27 13:29:49 +0200

    modified static in setttings

--- app/settings.py
+++ app/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/home/Bastian/weight_tracker/static'
+STATIC_URL = 'static' #/home/Bastian/weight_tracker/
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

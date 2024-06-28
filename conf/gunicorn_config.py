249b1b2b55bd3ba0c67c9fed784d68ea223b51bb
Bastian Katzmann <bastian.katzmann@web.de>
2024-06-28 18:29:02 +0200

    gunicorn & nginx test

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,4 +1,4 @@
 command = '/home/Bastian/.cache/pypoetry/virtualenvs/weight-tracker-w7sx_ZQD-py3.12/bin/gunicorn'
 pythonpath = '/home/Bastian/weight_tracker'
-bind = '192.168.178.100:8000'
+bind = 'unix:/home/Bastian/weight_tracker/weight_tracker.sock' #'192.168.178.100:8000'
 workers = 3

8ade5dc223b2ba6ba556e5b54ca096696069939d
Bastian Katzmann <bastian.katzmann@web.de>
2024-06-29 19:47:44 +0200

    csrf

--- app/settings.py
+++ app/settings.py
@@ -145,3 +145,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Domain registration for CSRF tokens
+CSRF_TRUSTED_ORIGINS = [env('CSRF_TRUSTED_ORIGINS')]

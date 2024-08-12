880848f9f7c57ed3b52b9a8b26e20bf7ef724078
Bastian Katzmann <bastian.katzmann@web.de>
2024-08-12 10:51:44 +0200

    shutdown test

--- shutdown/views.py
+++ shutdown/views.py
@@ -1,19 +1,15 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
 import subprocess
+import os
 
 @login_required
 @user_passes_test(lambda u: u.is_superuser)
 def shutdown(request):
     if request.method == 'POST':
         try:
-            result = subprocess.run(
-                ["sudo", "/sbin/shutdown", "-h", "now"],
-                capture_output=True,
-                text=True,
-                check=True
-            )
-            return HttpResponse(f'Shutdown command issued. Output: {result.stdout}')
-        except subprocess.CalledProcessError as e:
-            return HttpResponse(f'Failed to issue shutdown command: {e}', status=500)
+            os.system("sudo shutdown -h now")
+            return HttpResponse(f'Shutdown command issued.')
+        except:
+            return HttpResponse(f'Failed to issue shutdown command', status=500)
     return HttpResponse('Invalid request method.', status=405)

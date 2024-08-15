0f6dad06b4565a9670c325e3aeb4fad59cc7f565
Bastian Katzmann <bastian.katzmann@web.de>
2024-08-15 15:30:24 +0200

    fixed naming, inputs are added to database

--- weight/admin.py
+++ weight/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import Mass
 
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(Mass)
